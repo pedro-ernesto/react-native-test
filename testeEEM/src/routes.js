5ac28ec2ba97435c152b32c2b587f02ba324bd95
pedro <silveira-pedro@outlook.com>
2022-02-07 18:04:17 -0300

    criacao routes, configuracao do app, servico e estilos vazios

---
+++ testeEEM/src/routes.js
@@ -0,0 +1,25 @@
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import React from 'react';
+
+import Login from './pages/Login';
+
+const Stack = createNativeStackNavigator();
+
+export default function Routes() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRoutename="Login">
+        <Stack.Group screenOptions={{
+          headerStyle: { backgroundColor: '#333333' },
+          headerTintColor: '#ffffff',
+          headerTitleAlign: 'center',
+          headerBackTitleVisible: false,
+        }}
+        >
+          <Stack.Screen name="Login" component={Login} />
+        </Stack.Group>
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
---
+++ testeEEM/src/services/api.js
@@ -0,0 +1,9 @@
+import axios from 'axios';
+
+export const apiLogin = axios.create({
+
+});
+
+export const apiMessages = axios.create({
+
+});
